cf0f2a31567e60e4c4e96502f523017ed446676d
Nguyen Tran <nhtran@usc.edu>
2023-02-04 00:47:06 -0800

    first commit

---
+++ ArtSC/Controller/SignInViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SignInViewController.swift
+//  ArtSC
+//
+//  Created by Nguyen Tran on 2/4/23.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBAction func signInButtonDidTap(_ sender: Any) {
+        
+    }
+    
+}
