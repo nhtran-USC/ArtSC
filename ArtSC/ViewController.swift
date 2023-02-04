a7afcc0a9775e68df50a0621291ca3dec540cd5f
Nguyen Tran <nhtran@usc.edu>
2023-02-03 23:44:45 -0800

    Initial Commit

---
+++ ArtSC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArtSC
+//
+//  Created by Nguyen Tran on 2/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
